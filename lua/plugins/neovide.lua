3762317180fd4f97dcec3e95b66b8c5cd7e7618e
Kenneth Yang <kenneth@Kenneths-MacBook-Pro.local>
2024-08-03 19:16:58 -0700

    Start neovide at home dir

--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -1,5 +1,8 @@
 if not vim.g.neovide then return {} end
 
+-- Start in home dir.
+if vim.fn.getcwd() == "/" then vim.api.nvim_set_current_dir(vim.fn.expand "~") end
+
 return {
   "AstroNvim/astrocore",
   ---@type AstroCoreOpts
