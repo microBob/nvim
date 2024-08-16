75c62d7e3fc5070d9e436faddf3cfb38feeecfd5
Kenneth Yang <kenneth@Kenneths-MacBook-Pro.local>
2024-08-16 13:40:48 -0700

    nix and surround

--- lua/community.lua
+++ lua/community.lua
@@ -10,8 +10,12 @@ return {
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.recipes.neovide" },
 
+  -- Editing.
+  { import = "astrocommunity.motion.mini-surround" },
+
   -- Languages.
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.nix" },
   { import = "astrocommunity.pack.python-ruff" },
   -- import/override with your plugins folder
 }
